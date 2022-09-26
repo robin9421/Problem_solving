abf939ac4f9abdd5965be56bf7bb00b6441eb4ec
robin9421 <prajapatirobin123@gmail.com>
2022-09-26 09:25:09 -0400

    Added BFS and DFS

--- linkedList.js
+++ linkedList.js
@@ -1,18 +1,18 @@
-class Node {
-    constructor(val) {
-        this.val = val;
-        this.next = null;
-    }
- }
+// class Node {
+//     constructor(val) {
+//         this.val = val;
+//         this.next = null;
+//     }
+//  }
 
-const a = new Node('A');
-const b = new Node('B');
-const c = new Node('C');
-const d = new Node('D');
+// const a = new Node('A');
+// const b = new Node('B');
+// const c = new Node('C');
+// const d = new Node('D');
 
-a.next = b;
-b.next = c;
-c.next = d;
+// a.next = b;
+// b.next = c;
+// c.next = d;
 
 // const printLinkedList = (head) => {
 //     let current = head;
@@ -41,6 +41,80 @@ c.next = d;
 //     return searchLinkedList(head.next, target);
 // }
 
-const reverseLinkedList = (head) => {
-    
-}
+// -------------------------------------------------------------------------------------------------------------
+
+// ---------------------DEPTH FIRST SEARCH------------------------------
+// >>----------------------USING Iterative Approach-------------------------
+// const depthFirstPrint = (graph, source) => {
+//     const stack = [source];
+
+//     while(stack.length > 0){
+//         const current = stack.pop();
+//         console.log(current);
+
+//         for(let neighbor of graph[current]){
+//             stack.push(neighbor);
+//         }
+//     }
+// };
+
+// const graph = {
+//     a: ['c', 'b'],
+//     b: ['d'],
+//     c: ['e'],
+//     d: ['f'],
+//     e: [],
+//     f: []
+// };
+
+// depthFirstPrint(graph, 'a');
+
+// -------------------------------------------------------------------------
+// >>----------------------USING RECURSIVE METHOD---------------------------
+// const depthFirstPrintRec = (graph, source) => {
+//     console.log(source);
+
+//     for(let neighbor of graph[source]){
+//         depthFirstPrintRec(graph, neighbor);
+//     }
+// };
+
+// const graph = {
+//     a: ['c', 'b'],
+//     b: ['d'],
+//     c: ['e'],
+//     d: ['f'],
+//     e: [],
+//     f: []
+// };
+
+// depthFirstPrint(graph, 'a');
+// ---------------------------------------------------------------------------------------------------------
+
+// ---------------------BREADTH FIRST SEARCH------------------------------
+// >>----------------------USING Iterative Approach-------------------------
+// const breadthFirstPrint = (graph, source) => {
+//     const queue = [source];
+
+//     while(queue.length > 0){
+//         const current = queue.shift(); //removes first elements from array
+//         console.log(current);
+
+//         for(let neighbor of graph[current]){
+//             queue.push(neighbor);
+//         }
+//     }
+// };
+
+// const graph = {
+//     a: ['c', 'b'],
+//     b: ['d'],
+//     c: ['e'],
+//     d: ['f'],
+//     e: [],
+//     f: []
+// };
+
+// breadthFirstPrint(graph, 'a');
+
+// -------------------------------------------------------------------------
