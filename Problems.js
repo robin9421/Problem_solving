512f96f60fe55820ab4e3c1f01bdc413ba438346
robin9421 <prajapatirobin123@gmail.com>
2022-10-02 10:49:16 -0400

    Updated Add two strings and Add two linked lists

--- Problems.js
+++ Problems.js
@@ -88,3 +88,25 @@
 // };
 
 // --------------------------------------------------------------------------------------
+
+// -----------------Add Strings-------------------------------------
+// var addStrings = function(num1, num2) {
+//     let i = num1.length - 1;
+//     let j = num2.length - 1;
+//     let carry = 0;
+//     let sum = "";
+    
+//     while(i>=0 || j>=0 || carry>0) {
+//         let digit1 = i<0 ? 0 : num1.charAt(i) - '0';
+//         let digit2 = j<0 ? 0 : num2.charAt(j) - '0';
+//         let digitsum = digit1 + digit2 + carry;
+//         sum = (digitsum>=10 ? (digitsum % 10) : digitsum) + sum;
+//         carry = digitsum >=10 ? 1 :0
+        
+//         i--;
+//         j--;
+//     }
+//     return sum;
+// };
+
+// -----------------------------------------------------------------------------------------------
--- linkedList.js
+++ linkedList.js
@@ -220,40 +220,40 @@
 // ----------------------------------------------------------------------------------------------------------------------------------
 
 // ---------------------counting connected components----------------------------
-const connectedComponentsCount = (graph) => {
-    const visited = new Set();
-    let count = 0;
+// const connectedComponentsCount = (graph) => {
+//     const visited = new Set();
+//     let count = 0;
      
-    for (let node in graph) {
-        console.log(visited);
-        if(explore(graph, node, visited) == true) {
-            count += 1;
-        }
-    }
+//     for (let node in graph) {
+//         console.log(visited);
+//         if(explore(graph, node, visited) == true) {
+//             count += 1;
+//         }
+//     }
 
-    return count;
-};
+//     return count;
+// };
 
-const explore = (graph, current, visited) => {
-    if (visited.has(String(current))) return false;
+// const explore = (graph, current, visited) => {
+//     if (visited.has(String(current))) return false;
 
-    visited.add(String(current));
+//     visited.add(String(current));
 
-    for(let neighbor of graph[current]){
-        explore(graph, neighbor, visited);
-    }
+//     for(let neighbor of graph[current]){
+//         explore(graph, neighbor, visited);
+//     }
 
-    return true;
-};
+//     return true;
+// };
 
-const res = connectedComponentsCount({
-    0: [8, 1, 5],
-    1: [0],
-    5: [0, 8],
-    8: [0, 5],
-    2: [3, 4],
-    3: [2, 4],
-    4: [3, 2]
-});
+// const res = connectedComponentsCount({
+//     0: [8, 1, 5],
+//     1: [0],
+//     5: [0, 8],
+//     8: [0, 5],
+//     2: [3, 4],
+//     3: [2, 4],
+//     4: [3, 2]
+// });
 
 // console.log(res)
